7589a0b1b0792d84e6534cdfe8db5c8fee95467f
Suirad <suirad@users.noreply.github.com>
2019-10-10 19:46:39 -0500

    Update global step to use timer and have view only change in first person mode

--- sneakbob.lua
+++ sneakbob.lua
@@ -1,32 +1,23 @@
 -- Load config parameters
 local modpath = minetest.get_modpath(minetest.get_current_modname())
 
-function dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
-      end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
-end
-
 local view_offset = -1.5
-
+local timer = 0
 minetest.register_globalstep(function(dtime)
-    for  _,player in ipairs(minetest.get_connected_players()) do
-        local keys = player:get_player_control()
-        local offset = player:get_eye_offset()
-        if keys.sneak == true  and offset['y'] == 0 then
-            offset["y"] = view_offset
-            player:set_eye_offset(offset, offset)
-        elseif  keys.sneak == false  and offset['y'] == view_offset then
-            offset["y"] = 0
-            player:set_eye_offset(offset, offset)
+    timer = timer + dtime
+    if timer >= 0.1 then
+        for  _,player in ipairs(minetest.get_connected_players()) do
+            local keys = player:get_player_control()
+            local offset = player:get_eye_offset()
+            if keys.sneak == true  and offset['y'] == 0 then
+                offset["y"] = view_offset
+                player:set_eye_offset(offset, {x=0,y=0,z=0})
+            elseif  keys.sneak == false  and offset['y'] == view_offset then
+                offset["y"] = 0
+                player:set_eye_offset(offset, {x=0,y=0,z=0})
+            end
         end
+        timer = 0
     end
 end)
 
