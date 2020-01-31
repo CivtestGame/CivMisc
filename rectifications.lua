0d14f0e2fda7eef8f14d7eaa90b71bde1f631b12
R3R3R3 <>
2020-01-31 21:53:14 +0100

    Remove xdecor optional-but-hard dependency

--- rectifications.lua
+++ rectifications.lua
@@ -1,10 +1,15 @@
 
 -- Generalist file for rectifying the sins of other mods.
 
--- xdecor: disable the hammer recipe so that players can't repair their tools in
---         an xdecor:workbench.
-minetest.clear_craft({
-      output = "xdecor:hammer"
-})
+-- We use `register_on_mods_loaded` to avoid hard mod dependencies.
 
-minetest.debug("[CivMisc] Rectifications initialised.")
+minetest.register_on_mods_loaded(function()
+
+      -- xdecor: disable the hammer recipe so that players can't repair their tools in
+      --         an xdecor:workbench.
+      if minetest.get_modpath("xdecor") then
+         minetest.clear_craft({ output = "xdecor:hammer" })
+      end
+
+      minetest.debug("[CivMisc] Rectifications initialised.")
+end)
