89e8039397ce16d8bc559f2b53a61efd0a0130de
R3R3R3 <>
2020-02-04 21:41:32 +0100

    Make fall damage just a little bit more forgiving

--- damagemod.lua
+++ damagemod.lua
@@ -2,7 +2,7 @@
 minetest.register_on_player_hpchange(function(player, hp_change, reason)
       if hp_change < 0 and reason then
          if reason.type == "fall" then
-            return hp_change * 8
+            return hp_change * 7
          elseif reason.type == "node_damage" then
             return hp_change * 8
          elseif reason.type == "drown" then
