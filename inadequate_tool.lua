32dad7408d0d9c90c2dd4a348d6e807b88faf782
igotyou <>
2020-03-31 19:29:12 +0200

    /cti doesn't trigger inadequate tool warning

--- inadequate_tool.lua
+++ inadequate_tool.lua
@@ -10,7 +10,8 @@ minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
    local held_def = core.registered_items[held:get_name()]
 
    if (has_citadella
-          and ct.player_modes[pname] == ct.PLAYER_MODE_REINFORCE)
+          and (ct.player_modes[pname] == ct.PLAYER_MODE_REINFORCE
+                  or ct.player_modes[pname] == ct.PLAYER_MODE_INFO))
       or held:get_name():find("sword")
    then
       return
