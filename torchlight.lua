7e75d663133f347f2611aebf3b4426c48c540d47
R3R3R3 <>
2020-07-23 17:55:10 +0200

    Reenable glowsticks

--- torchlight.lua
+++ torchlight.lua
@@ -90,42 +90,42 @@ minetest.register_on_leaveplayer(function(player, timeout)
 end)
 
 -- detect player active item
-local timer = 0
-minetest.register_globalstep(function(dtime)
-    timer = timer + dtime
-    if timer < 0.5 then
-       return
-    end
-    timer = 0
-    for _,player in ipairs(minetest.get_connected_players()) do
-        local inv = player:get_inventory()
-        local ppos = player:getpos()
-        local holding = false
+-- local timer = 0
+-- minetest.register_globalstep(function(dtime)
+--     timer = timer + dtime
+--     if timer < 0.5 then
+--        return
+--     end
+--     timer = 0
+--     for _,player in ipairs(minetest.get_connected_players()) do
+--         local inv = player:get_inventory()
+--         local ppos = player:getpos()
+--         local holding = false
 
-        if player:get_wielded_item():get_name() == "default:torch" then
-            holding = true
-            -- minetest.chat_send_all("Player is holding torch")
-        end
+--         if player:get_wielded_item():get_name() == "default:torch" then
+--             holding = true
+--             -- minetest.chat_send_all("Player is holding torch")
+--         end
 
-        local found = false
-        for _,obj in ipairs(minetest.get_objects_inside_radius(ppos, 1)) do
-            local entity = obj:get_luaentity()
-            if entity and entity.name == "civmisc:torchlight" then
-                found = true
-                if not holding then
-                    entity:destroy()
-                end
-            end
-        end
+--         local found = false
+--         for _,obj in ipairs(minetest.get_objects_inside_radius(ppos, 1)) do
+--             local entity = obj:get_luaentity()
+--             if entity and entity.name == "civmisc:torchlight" then
+--                 found = true
+--                 if not holding then
+--                     entity:destroy()
+--                 end
+--             end
+--         end
 
-        if holding and not found then
-            -- spawn
-            local tlight = minetest.add_entity(ppos, "civmisc:torchlight", nil)
-            tlight:set_attach(player, "Arm_Right", {0,0,0}, {0,0,0})
-        end
+--         if holding and not found then
+--             -- spawn
+--             local tlight = minetest.add_entity(ppos, "civmisc:torchlight", nil)
+--             tlight:set_attach(player, "Arm_Right", {0,0,0}, {0,0,0})
+--         end
 
-    end
-end)
+--     end
+-- end)
 
 -- Glowstick, adapted from Nightscapes mod by Darkflame999 https://forum.minetest.net/viewtopic.php?f=9&t=24555
 
@@ -139,30 +139,29 @@ minetest.register_entity("civmisc:glowstick_entity", {
 		textures = {"civmisc_glowstick.png"},
 		spritediv = {x=1, y=1},
 		initial_sprite_basepos = {x=0, y=0},
-		is_visible = "true",
+		is_visible = true,
 		timer = 0,
 		physical = true,
 		collide_with_objects = true,
 	},
 	on_activate = function(self, staticdata, dtime_s)
-		last_pos = self.object:get_pos()
+           self.last_pos = self.object:get_pos() or {x=-1, y=-1, z=-1}
 	end,
-	last_pos = {x=-1, y=-1, z=-1},
 	on_step = function(self, dtime)
-		temp = self.object:getpos()
+		local temp = self.object:getpos()
 		if self.last_pos.y ~= temp.y then
 			minetest.set_node(self.last_pos, {name="air"})
 			self.last_pos = temp
 			minetest.set_node(self.last_pos, {name = "civmisc:torchlight"})
 		end
-		node = minetest.get_node({x=self.last_pos.x, y=self.last_pos.y-0.3, z=self.last_pos.z})
+		local node = minetest.get_node({x=self.last_pos.x, y=self.last_pos.y-0.3, z=self.last_pos.z})
 		if minetest.registered_nodes[node.name].walkable then
 			self.object:setvelocity({x=0, y=-2, z=0})
 			self.object:setacceleration({x=0, y=-10, z=0})
 		end
 	end,
 	on_punch = function(self, hitter)
-		nodes_in_area = minetest.find_nodes_in_area({x=self.last_pos.x-1, y=self.last_pos.y-1, z=self.last_pos.z-1}, {x=self.last_pos.x+1, y=self.last_pos.y+1, z=self.last_pos.z+1}, {"civmisc:torchlight"})
+		local nodes_in_area = minetest.find_nodes_in_area({x=self.last_pos.x-1, y=self.last_pos.y-1, z=self.last_pos.z-1}, {x=self.last_pos.x+1, y=self.last_pos.y+1, z=self.last_pos.z+1}, {"civmisc:torchlight"})
 		for i=1, table.getn(nodes_in_area) do
 			minetest.set_node(nodes_in_area[i], {name = "air"})
 		end
@@ -175,8 +174,8 @@ minetest.register_craftitem("civmisc:glowstick", {
 	description = "Glowstick",
 	inventory_image = "civmisc_glowstick.png",
 	on_drop = function(itemstack, dropper, pos)
-		obj = minetest.add_entity({x=pos.x, y=pos.y+1.3, z=pos.z}, "civmisc:glowstick_entity")
-		dir = dropper:get_look_dir()
+		local obj = minetest.add_entity({x=pos.x, y=pos.y+1.3, z=pos.z}, "civmisc:glowstick_entity")
+		local dir = dropper:get_look_dir()
 		obj:setvelocity({x=dir.x*12, y=dir.y*10, z=dir.z*12})
 		obj:setacceleration({x=dir.x*-3, y=-10, z=dir.z*-3})
 		itemstack:take_item()
