5e51298b33a010e61d41a28958750e1167b008f3
R3R3R3 <>
2020-08-28 17:06:45 +0200

    Inspection Gloves cleanup

--- inventory_inspection.lua
+++ inventory_inspection.lua
@@ -1,106 +1,147 @@
---Adds an item which allows a player to inspect another persons inventory
---Idea:add gate block which only allows players with specific items in
+-- Adds an item which allows a player to inspect another persons inventory
+-- Idea: add gate block which only allows players with specific items in
 
 local perm_table = {}
-local invite_tabe = {}
+local invite_table = {}
 
-local function disallow(name)
-    perm_table[name] = nil
+local function disallow_inspect(name)
+   perm_table[name] = nil
 end
 
 local function get_inv_formspec(player_name, inv_name)
-
    local open = {
-    "size[8,5.5]",
-    "label[0,0;", player_name, "'s Inventory]",
-    "list[detached:", inv_name, ";main2;0,0.5;8,3;]",
-    "list[detached:", inv_name,";main;0,4.5;8,1]",
- }
- return table.concat(open, "")
+      "size[8,5.5]",
+      "label[0,0;", player_name, "'s Inventory]",
+      "list[detached:", inv_name, ";main2;0,0.5;8,3;]",
+      "list[detached:", inv_name,";main;0,4.5;8,1]",
+   }
+   return table.concat(open, "")
 end
 
-local function use(itemstack, user, pointed_thing)
-    if pointed_thing.ref and pointed_thing.ref:is_player() then
-        local target_name = pointed_thing.ref:get_player_name()
-        if perm_table[target_name] and (perm_table[target_name] == true or perm_table[target_name] == user:get_player_name()) then
-            local inv_name = target_name .. "_inspection"
-            local inv = minetest.get_player_by_name(target_name):get_inventory()
-            local inspection_inv = minetest.create_detached_inventory(inv_name, {
-                allow_move = function(inv, from_list, from_index, to_list, to_index, count, player)
-                    return 0
-                end,
+local function on_inspection_gloves_use(itemstack, user, pointed_thing)
+   if not pointed_thing.ref
+      or not pointed_thing.ref:is_player()
+   then
+      return
+   end
 
-                allow_put = function(inv, listname, index, stack, player)
-                    return 0
-                end,
+   local target_name = pointed_thing.ref:get_player_name()
+   local user_name = user:get_player_name()
 
-                allow_take = function(inv, listname, index, stack, player)
-                    return 0
-                end,
-            })
-            local main = inv:get_list("main")
-            local main2 = inv:get_list("main2")
-            inspection_inv:set_list("main", main)
-            inspection_inv:set_list("main2", main2)
-            minetest.show_formspec(user:get_player_name(), "inv_inspection", get_inv_formspec(target_name,inv_name))
-            minetest.chat_send_player(target_name, user:get_player_name() .. " is inspecting your inventory.")
-            perm_table[target_name] = nil
-            itemstack:take_item()
-            return itemstack
-        else
-            minetest.chat_send_player(user:get_player_name(), target_name .. " has not allowed you to inspect their inventory.")
-            minetest.chat_send_player(target_name, user:get_player_name() .. " tried to inspect your inventory, but you have not allowed it. Use /allow_inspection to allow it.")
-            invite_tabe[target_name] = user:get_player_name()
-        end
-    end
+   if not perm_table[target_name] then
+      minetest.chat_send_player(
+         user_name,
+         target_name .. " has not allowed you to inspect their inventory."
+      )
+      minetest.chat_send_player(
+         target_name,
+         user_name .. " tried to inspect your inventory, but you have "
+            .. "not allowed it.\n"
+            .. "Use '/allow_inspection " .. user_name .. "' to allow it."
+      )
+      invite_table[target_name] = user_name
+      return
+   end
+
+   local inv_name = target_name .. "_inspection"
+   local inv = minetest.get_player_by_name(target_name):get_inventory()
+   local inspection_inv = minetest.create_detached_inventory(
+      inv_name,
+      {
+         allow_move = function() return 0 end,
+         allow_put = function() return 0 end,
+         allow_take = function() return 0 end,
+      }
+   )
+
+   local main = inv:get_list("main")
+   local main2 = inv:get_list("main2")
+   inspection_inv:set_list("main", main)
+   inspection_inv:set_list("main2", main2)
+
+   minetest.show_formspec(
+      user_name, "inv_inspection", get_inv_formspec(target_name, inv_name)
+   )
+
+   minetest.chat_send_player(
+      target_name, user_name .. " is inspecting your inventory."
+   )
+
+   perm_table[target_name] = nil
+
+   itemstack:take_item()
+   return itemstack
 end
 
-minetest.register_craftitem("civmisc:inspection_gloves", {
-    description = "Inspection gloves",
-    on_use = use,
-    inventory_image = "gloves.png"
-})
+minetest.register_craftitem(
+   "civmisc:inspection_gloves",
+   {
+      description = "Inspection Gloves",
+      on_use = on_inspection_gloves_use,
+      inventory_image = "civmisc_inspection_gloves.png"
+   }
+)
 
 minetest.register_chatcommand(
    "allow_inspection",
    {
-      params = "[<params>]",
-      description = "Allows a target(or anyone) to inspect your inventory.",
+      params = "[<target>]",
+      description = "Allows a target (or anyone) to inspect your inventory.",
       func = function(sender, params)
-        local splitResult = params:split(" ")
-        local target = splitResult[1]
-        if target == nil then target = invite_tabe[sender] end
-        local time = tonumber(splitResult[2]) or 30
-        if minetest.get_player_by_name(sender) then
-            if time < 1 then time = 1 end --Don't know if it's required, but just want to be sure.
-            if time > 600 then time = 600 end
-            minetest.after(time, disallow, sender)
-            if target == nil then
-                minetest.chat_send_player(sender, "No target specified")
-                return false
-            else
-                minetest.chat_send_player(sender, target .. " can now inspect your inventory for the next " .. time .. " seconds")
-                perm_table[sender] = target
-            end
-        else
+         local player = minetest.get_player_by_name(sender)
+         if not player then
             return false
-        end
+         end
+         sender = player:get_player_name()
+
+         local split_result = params:split(" ")
+         local target = split_result[1]
+         if not target then
+            target = invite_table[sender]
+         end
+
+         local time = tonumber(split_result[2]) or 30
+
+         -- Clamp potential time inputs between 1s and 5mins.
+         if time < 1 then time = 1 end
+         if time > 300 then time = 300 end
+
+         minetest.after(time, disallow_inspect, sender)
+
+         if not target then
+            return false, "No target specified"
+         end
+
+         minetest.chat_send_player(
+            sender, target .. " can now inspect your inventory for "
+               .. " the next " .. time .. " seconds"
+         )
+
+         perm_table[sender] = target
+         return true
       end
    }
 )
 
-
-
 minetest.register_chatcommand(
    "disallow_inspection",
    {
-      description = "Disallows anyone from inspecting your inventory.",
-      func = function(sender, target)
-        if minetest.get_player_by_name(sender) then
-            disallow(sender)
-        else
+      params = "",
+      description = "Disallows inspections of your inventory.",
+      func = function(sender)
+         local player = minetest.get_player_by_name(sender)
+         if not player then
             return false
-        end
+         end
+
+         local pname = player:get_player_name()
+
+         disallow_inspect(pname)
+
+         minetest.chat_send_player(
+            pname, "You disallowed inspections of your inventory."
+         )
+         return true
       end
    }
 )
